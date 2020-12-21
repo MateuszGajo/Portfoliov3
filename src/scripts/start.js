deabde2201e976d8cd0e0db9a99442f695303482
Mateusz <mateusz.gajo9@wp.pl>
2020-12-21 23:43:45 +0100

    app intialized, basic animation created

---
+++ src/scripts/start.js
@@ -0,0 +1,95 @@
+setTimeout(() => {
+  document
+    .querySelector(".start__line--content")
+    .classList.add("start__line--content--active");
+}, 2400);
+
+setTimeout(() => {
+  document
+    .querySelector(".start__line--content")
+    .classList.add("start__line--active");
+}, 3000);
+
+setTimeout(() => {
+  document
+    .querySelector(".start__line--downbar")
+    .classList.add("start__line--downbar--active");
+}, 3400);
+
+setTimeout(() => {
+  document
+    .querySelector(".start__downbar__text__line--title")
+    .classList.add("start__downbar__text__line--draw-active");
+}, 3500);
+
+setTimeout(() => {
+  document
+    .querySelector(".start__downbar__text--title")
+    .classList.add("start__downbar__text--visible");
+}, 3600);
+
+setTimeout(() => {
+  document
+    .querySelector(".start__downbar__text__line--author")
+    .classList.add("start__downbar__text__line--draw-active");
+}, 3700);
+
+setTimeout(() => {
+  document
+    .querySelector(".start__downbar__text--author")
+    .classList.add("start__downbar__text--visible");
+}, 3800);
+
+setTimeout(() => {
+  document
+    .querySelector(".start__line--downbar")
+    .classList.add("start__line--active");
+}, 4000);
+
+setTimeout(() => {
+  document
+    .querySelector(".start__downbar__text__line--title")
+    .classList.add("start__downbar__text__line--active");
+}, 4100);
+
+setTimeout(() => {
+  document
+    .querySelector(".start__downbar__text__line--author")
+    .classList.add("start__downbar__text__line--active");
+}, 4300);
+
+setTimeout(() => {
+  document
+    .querySelector(".start__downbar__text__overlay--title")
+    .classList.add("start__downbar__text__overlay--active");
+}, 4500);
+
+setTimeout(() => {
+  document
+    .querySelector(".start__downbar__text__overlay--author")
+    .classList.add("start__downbar__text__overlay--active");
+}, 4700);
+
+setTimeout(() => {
+  document
+    .querySelector(".start__downbar__text__line--author")
+    .classList.remove("start__downbar__text__line--draw-active");
+}, 5800);
+
+setTimeout(() => {
+  document
+    .querySelector(".start__downbar__text__line--title")
+    .classList.remove("start__downbar__text__line--draw-active");
+}, 5900);
+
+setTimeout(() => {
+  document
+    .querySelector(".start__line--downbar")
+    .classList.remove("start__line--downbar--active");
+}, 6000);
+
+setTimeout(() => {
+  document
+    .querySelector(".start__line--content")
+    .classList.remove("start__line--content--active");
+}, 6300);
