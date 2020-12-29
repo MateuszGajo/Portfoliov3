1ea7e15c0a2d544e4eb0e356bd85d960a4f43c37
Mateusz <mateusz.gajo9@wp.pl>
2020-12-29 20:56:12 +0100

    technologies animations improved, icons changed

--- src/scripts/skills.js
+++ src/scripts/skills.js
@@ -7,7 +7,16 @@ const SkillsDescriptionTextEl = document.querySelector(
   ".skills__description__text"
 );
 const techonologiesLine = document.querySelector(".skills__technologies__line");
+const technologiesLineOverlay = document.querySelector(
+  ".skills__technologies__line__overlay"
+);
 const technologiesEl = document.querySelector(".skills__technologies");
+const skillsItems = document.querySelectorAll(
+  ".skills__technologies__wrapper__container"
+);
+const itemPoint = document.querySelector(
+  ".skills__technologies__wrapper__container__point"
+);
 
 const skillsStartPosition = skillsSection.getBoundingClientRect().y;
 const endOfSkillsSection = skillsStartPosition + windowHeight;
@@ -24,20 +33,18 @@ skillsDescriptionTitleEl.style.transform = `translate(calc(30vw - 220px), ${
 const setAnimation = (el, type, className, time = 0, styles) => {
   switch (type) {
     case "add": {
-      setTimeout(() => {
+      return setTimeout(() => {
         el.classList.add(className);
       }, time);
-      break;
     }
     case "remove": {
-      setTimeout(() => {
+      return setTimeout(() => {
         el.classList.remove(className);
       }, time);
-      break;
     }
     case "style": {
-      setTimeout(() => {
-        el.style[styles.key] = styles.name;
+      return setTimeout(() => {
+        el.style[styles.key] = styles.value;
       }, time);
     }
   }
@@ -47,7 +54,7 @@ const startSection = () => {
   setAnimation(skillsDescriptionTitleEl, "add", "opacity-visible", 200);
   setAnimation(skillsDescriptionTitleEl, "style", "", 600, {
     key: "transform",
-    name: "translate(0,0) scale(1)",
+    value: "translate(0,0) scale(1)",
   });
   setAnimation(
     SkillsDescriptionTextEl,
@@ -70,12 +77,13 @@ const startSection = () => {
     2100
   );
 };
+setAnimation(technologiesLineOverlay, "style", "", 3100, {
+  key: "height",
+  value: windowHeight / 2 + "px",
+});
 startSection();
-let isSectionStart = false;
 
-const skillsItems = document.querySelectorAll(
-  ".skills__technologies__wrapper__container"
-);
+let isSectionStart = false;
 
 const skillsItemsSorted = [].map
   .call(skillsItems, function (el) {
@@ -85,46 +93,81 @@ const skillsItemsSorted = [].map
     return a.offsetTop - b.offsetTop;
   });
 
-// aa.forEach((item) => console.log(item.offsetTop));
-
-let counter = 0;
-
+let i = 0;
+const last = skillsItemsSorted.length;
+let animation = {
+  index: null,
+  date: null,
+};
 technologiesEl.addEventListener("scroll", (e) => {
-  // console.log(skillsItemsSorted[1].offsetTop);
-  // console.log(skillsItemsSorted[1].offsetHeight);
-  console.log(e.target.scrollTop + windowHeight / 2);
-  console.log(skillsItemsSorted[1].offsetTop);
-  console.log(
-    skillsItemsSorted[1].offsetTop + skillsItemsSorted[1].offsetHeight / 2 - 100
-  );
-  if (
-    e.target.scrollTop + windowHeight / 2 >
-      skillsItemsSorted[1].offsetTop +
-        skillsItemsSorted[1].offsetHeight / 2 -
-        100 &&
-    e.target.scrollTop + windowHeight / 2 <
-      skillsItemsSorted[1].offsetTop +
-        skillsItemsSorted[1].offsetHeight / 2 +
-        100
-  ) {
-    console.log("jesteśmy tu");
-    // e.scrollBy({
+  console.log(i);
+  const scrollCenter = windowHeight / 2 + e.target.scrollTop;
+  const lineScroll = windowHeight / 1.5 + e.target.scrollTop;
+  const elementCenter =
+    skillsItemsSorted[i].offsetTop + skillsItemsSorted[i].offsetHeight / 2;
+  const lastElementPosition =
+    skillsItemsSorted[last - 1].offsetTop +
+    skillsItemsSorted[last - 1].offsetHeight / 2;
+  const itemPointHeight = itemPoint.offsetHeight;
 
-    // })
-    // console.log(skillsItems[2].offsetTop);
-    // technologiesEl.scrollBy({
-    //   top: skillsItems[2].offsetTop,
-    //   behavior: "smooth",
-    // });
-    // technologiesEl.scrollTop = skillsItems[2].offsetTop;
-  }
   if (
-    windowHeight / 1.5 + e.target.scrollTop > windowHeight &&
-    windowHeight / 1.5 + e.target.scrollTop <
-      e.target.scrollHeight + windowHeight * 2
-  )
-    techonologiesLine.style.height =
-      windowHeight / 1.5 + e.target.scrollTop + "px";
+    animation.index &&
+    new Date().getTime() - animation.date.getTime() < 300
+  ) {
+    clearTimeout(animation.index);
+  }
+
+  if (lineScroll > lastElementPosition) {
+    if (scrollCenter > lastElementPosition) {
+      technologiesLineOverlay.style.height = lastElementPosition - 30 + "px";
+      setAnimation(
+        skillsItemsSorted[i],
+        "add",
+        "skills__technologies__wrapper__container--active",
+        0
+      );
+    } else technologiesLineOverlay.style.height = scrollCenter - 30 + "px";
+    techonologiesLine.style.height = lastElementPosition - 30 + "px";
+  } else if (scrollCenter > elementCenter - itemPointHeight / 2) {
+    technologiesLineOverlay.style.height =
+      elementCenter - itemPointHeight / 2 + "px";
+
+    setAnimation(
+      skillsItemsSorted[i],
+      "add",
+      "skills__technologies__wrapper__container--active",
+      1000
+    );
+
+    animation.index = setAnimation(technologiesLineOverlay, "style", "", 300, {
+      key: "height",
+      value: scrollCenter + "px",
+    });
+    animation.date = new Date();
+
+    if (i < last - 1) i++;
+  } else {
+    technologiesLineOverlay.style.height = scrollCenter - 30 + "px";
+    techonologiesLine.style.height = lineScroll + "px";
+  }
+
+  if (i > 0) {
+    const previousElementCenter =
+      skillsItemsSorted[i - 1].offsetTop +
+      skillsItemsSorted[i - 1].offsetHeight / 2;
+
+    if (scrollCenter > previousElementCenter + windowHeight / 2.45) {
+      skillsItemsSorted[i - 1].classList.add(
+        "skills__technologies__wrapper__container--deactive"
+      );
+    }
+  }
+
+  if (
+    lineScroll > windowHeight &&
+    lineScroll < e.target.scrollHeight + windowHeight * 2
+  ) {
+  }
 });
 
 document.addEventListener("scroll", (e) => {
